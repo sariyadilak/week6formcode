4e8f5a5d3173ea88516d3b8a7c704e9c3298e6e9
sariyadilak <ariyadilak.s@gmail.com>
2018-04-19 13:51:20 -0400

    add uploadjs

--- ucesriy/www/js/uploadData.js
+++ ucesriy/www/js/uploadData.js
@@ -1,3 +1,30 @@
 function startDataUpload() {
 alert ("start data upload");
+var name = document.getElementById("name").value;
+var surname = document.getElementById("surname").value;
+var module = document.getElementById("module").value;
+alert(name + " "+ surname + " "+module);
+var postString = "name="+name +"&surname="+surname+"&module="+module;
+processData(postString);
+}
+
+
+//Add an AJAX call and response method to code in uploadData.js
+var client;
+function processData(postString) {
+client = new XMLHttpRequest();
+client.open('POST','http://developer.cege.ucl.ac.uk:30281/uploadData',true);
+client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+client.onreadystatechange = dataUploaded;
+client.send(postString);
+}
+// create the code to wait for the response from the data server, and process the
+response once it is received
+function dataUploaded() {
+// this function listens out for the server to say that the data is ready - i.e.
+has state 4
+if (client.readyState == 4) {
+// change the DIV to show the response
+document.getElementById("dataUploadResult").innerHTML = client.responseText;
+}
 }
